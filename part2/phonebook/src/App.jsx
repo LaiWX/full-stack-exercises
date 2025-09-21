994665ab00660b3d9f8e6f1b4f14a93b1a2b8816
lwx12 <lwx128@live.com>
2025-09-21 19:54:47 +0800

    complete the 2.11 exercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filterString, onFilterStringChange }) => {
     return (
@@ -59,15 +60,18 @@ const Person = ({ name, number}) => {
     )
 }
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456'},
-        { name: 'Ada Lovelace', number: '39-44-5323523'},
-        { name: 'Dan Abramov', number: '12-43-234345'},
-        { name: 'Mary Poppendieck', number: '39-23-6423122'}
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filterString, setFilterString] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then((res) => {
+                setPersons(res.data)
+            })
+    },[])
 
     const onNameSubmit = (e) => {
         e.preventDefault()
