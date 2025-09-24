52032d2049a64b6cf53c2f5e09d08cb5dcda876d
weixuan_lai <weixuan_lai@tianma.cn>
2025-09-24 17:11:39 +0800

    complete 3.7~3.8 exercises

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', (req) => {
+    if (req.method !== 'POST') {
+        return ''
+    } else {
+        return JSON.stringify(req.body)
+    }
+})
+morgan.format('tiny-with-body', ':method :url :status :res[content-length] - :response-time ms :body')
+
+app.use(morgan('tiny-with-body'))
 
 let persons = [
     {
